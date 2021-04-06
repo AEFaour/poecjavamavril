a19ef72dd8a6ad9ee676df7c4d7320ec1fadbf1f
aefaour <anas-emmanuel.faour@orange.fr>
2021-04-06 17:07:21 +0200

    poec java init first

---
+++ 01_Introduction/src/main/java/fr/poecjava/javase/Simple.java
@@ -0,0 +1,20 @@
+package fr.poecjava.javase; // On va préferer la notation de reverse DNS ==> Domain Name Server
+
+public class Simple {
+	public static void main(String[] args) {
+		System.out.println("Le java a l'air très fun !!!");
+		System.out.print("Programmer est un exercice interessant !!!"); // ==> C'est une inscruction Java
+		System.out.print("C'est déjà la fin du cours");
+	}
+}
+
+// Ceci est commentaire sur une ligne en Java --> Il n'est pas interprêté 
+// Une instruction java se termine par un ;
+// Le nom d'une classe en java commence par une lettre majuscule 
+// Le nom d'une méthode commence par une lettre miniscule. Si le nom est composé, on utilise la notation camel case
+// Un programme executable doit posseder la méthode "public static  void main(String[] args)"
+// L méthode print & println permettent de faire un affichage dans la console
+// Le code java s'execute sequentiellement
+// print me permet de faire un affichage sur une ligne 
+// println = print + ln --> ln == new line
+// Java est très sensible à la case 
