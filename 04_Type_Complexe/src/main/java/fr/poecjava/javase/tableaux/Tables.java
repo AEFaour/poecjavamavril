3a9a492b84fc97db87ddad44b80931b7311f29ae
aefaour <anas-emmanuel.faour@orange.fr>
2021-04-07 16:57:38 +0200

    Tables plus

--- 04_Type_Complexe/src/main/java/fr/poecjava/javase/tableaux/Tables.java
+++ 04_Type_Complexe/src/main/java/fr/poecjava/javase/tableaux/Tables.java
@@ -26,13 +26,20 @@ public class Tables {
 		
 		System.out.printf("notes[2] = %s\n", notes[2]);
 		
-		char[] caracteres = {'J', 'o', 'a', 'c', 'h', 'i', 'm'};
+		char[] caracteres = {'J', 'o', 'a', 'c', 'h', 'i', 'm'}; // Définition explicite d'un tableau
 		
 		System.out.printf("caracteres = %s\n", Arrays.toString(caracteres));
 		
-		System.out.println("caracteres = " + Arrays.toString(caracteres));
+		System.out.println("caracteres = " + Arrays.toString(caracteres)); // opérateur de concatenation
+		System.out.println(2.5 + 3); // opérateur polymorphe
+		System.out.println("addition = " + ( 2.5 + 3)); 
 		
+		int valeurs []; // Autre définition de tableau à nes pas priviliger
 		
+		System.out.println("nbCarateres = " + caracteres.length);
+		
+		System.out.println(caracteres[4]);
+		System.out.println(caracteres[caracteres.length-3]);
 
 	}
 
